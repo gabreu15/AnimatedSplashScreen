46b6c0159a1bf5fc798938d0d6e9614f7e1aafda
gabreu15 <130008071+gabreu15@users.noreply.github.com>
2023-11-26 15:28:47 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/trabalhosplashscreen/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.trabalhosplashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val motionLayout : MotionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity))
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+
+    }
+}
